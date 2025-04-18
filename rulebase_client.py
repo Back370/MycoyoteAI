abfb73ecd86883f325c4bc5d3f2d9a4c378b01c5
carbood <168190155+Back370@users.noreply.github.com>
2025-04-18 11:38:02 +0900

    ルールベースAI完成

--- rulebase_client.py
+++ rulebase_client.py
@@ -109,172 +109,45 @@ def convert_card(self, cards, Is_othersum, deck):
 class Player:
     def __init__(self):
         self.mycard = None
-        self.score = 0
-        self.visible = 0
-        self.expect_card = None
-        self.expect = 0
         self.others_expect_sum = 0
         self.expect_cards = []
         self.deck = Deck()
         self.players_life = {} #{"player_name": life}
         self.previous_round_num = 0
+        self.expect_sum = 0
+        self.is_double_card = False #二倍するかどうか
+        self.is_shuffle_card = False
 
     def ai_turn(self, others_info, sum, action, round_num):
+        #　黒背景の０を引いたときに山札をリセットする
+        if(self.is_shuffle_card):
+            self.deck.reset()
+            self.deck.cashed_cards = [] #山札に戻すカードをリセットする
+            self.is_shuffle_card = False
+
         # AIの行動を決定するロジックを実装する
         # 全員の手札を配列に格納する
         now_round_num = round_num
         if now_round_num != self.previous_round_num:
             self.previous_round_num = now_round_num
             self.expect_cards = [card for card in others_info["card_info"]] # 他プレイヤーの手札を格納する
+            for card in self.expect_cards:
+                if len(self.deck.cards) > 0:
+                    self.deck.cards.remove(card) # 場のカードを山札から削除する
+                else:
+                    random.shuffle(self.deck.cashed_cards) #山札に戻すカードをシャッフルする
+                    #山札が空になったら、捨て札を山札に追加する
+                    self.deck.cards = self.deck.cashed_cards.copy()
+                    self.deck.cashed_cards = []
+
             self.mycard = self.deck.draw() # 自分の手札を引く
             self.expect_cards.append(self.mycard) # 自分の手札を格納する
+            self.deck.cashed_cards.append(self.expect_cards) # 自分の手札を山札に戻す
             sorted(self.expect_cards, reverse=True) # 降順にソートする
-            convert_card(self.expect_cards, False, self.deck) # 自分の手札を変換する
-            self.others_expect_sum = convert_card(self.expect_cards, True, self.deck) # プレイヤーの手札を変換する
-            
-        
-      
-
-
-   
-
-        if self.visible > game_state["call"]:
-            raise_num = random.randint(1, self.visible - game_state["call"])
-            return True, raise_num
+            self.expect_sum = convert_card(self.expect_cards, False, self.deck) # type: ignore # 場のカードの合計値を計算する
         else:
-            if self.expect > game_state["call"]:
-                raise_num = random.randint(1, self.expect - game_state["call"])
-                return True, raise_num
+            #もし宣言できる数が予測値を超えないなら,その幅でランダムに選ぶ 
+            if len(action[0:self.expect_sum]) > 0:
+                return random.choice(action[0:self.expect_sum])
             else:
-                # 初手でコールストップはできない
-                if game_state["call"] == 0:
-                    return True, 1
-                return False, 0
-
-    # プレイヤーに見えている数だけ計算
-    def calc_visible(self, players):
-        Ace_count_each = 0
-        for other in players:
-            if self != other:
-                self.visible += get_card_value(other.card)
-                if other.card.value == "A":
-                    Ace_count_each += 1
-        self.visible *= 2**Ace_count_each
-
-    # プレイヤーの予想値を計算
-    def calc_expect(self, players, deck):
-        self.expect_card = random.choice(
-            deck.cards + [self.card]
-        )  # 完全に残りのデッキを覚えている
-        Ace_count_each = 0
-        self.expect += get_card_value(self.expect_card)
-        if self.expect_card.value == "A":
-            Ace_count_each += 1
-        for other in players:
-            if self != other:
-                self.expect += get_card_value(other.card)
-                if other.card.value == "A":
-                    Ace_count_each += 1
-        self.expect *= 2**Ace_count_each
-
-def display_cards_except_you(players):
-    card_displays = []
-    for player in players:
-        if player.name != "You":
-            card_displays.append(player.card.ascii_rep())
-    for i in range(7):
-        print(" ".join(card[i] for card in card_displays))
-    for player in players:
-        if player.name != "You":
-            print(f"{player.name:^9}", end=" ")
-    print()
-
-
-def display_cards(players):
-    card_displays = [
-        player.card.ascii_rep() if player.card else [" " * 9] * 7 for player in players
-    ]
-    for i in range(7):
-        print(" ".join(card[i] for card in card_displays))
-    for player in players:
-        print(f"{player.name:^9}", end=" ")
-    print()
-
-
-def play_round(players, deck, start_index):
-    while len(players) > 1:
-        print(
-            f"\n{Fore.MAGENTA}***********************New Game!!***********************"
-        )
-        game_state = {"sum": 0, "call": 0, "Ace_count": 0}
-
-        for player in players:
-            player.visible = 0
-            player.expect = 0
-            player.draw_card(deck)
-
-        # 各プレイヤーの値を計算
-        for i in players:
-            i.calc_visible(players)
-            i.calc_expect(players, deck)
-
-        # フィールドの合計値を計算
-        for i in players:
-            game_state["sum"] += get_card_value(i.card)
-            if i.card.value == "A":
-                game_state["Ace_count"] += 1
-        game_state["sum"] *= 2 ** game_state["Ace_count"]
-
-        time.sleep(2)
-
-        display_cards_except_you(players)
-        time.sleep(4)
-        stopper = None
-        while True:
-            for i in range(len(players)):
-                index = (i + start_index) % len(players)
-                player = players[index]
-                print(f"\n{Fore.CYAN}**********************************************")
-                print(f"{Fore.YELLOW}It's {player.name}'s turn!")
-                continue_call, raise_amount = player.play_turn(game_state)
-                if continue_call:
-                    game_state["call"] += raise_amount
-                    print(f"{Fore.GREEN}Call is now {game_state['call']}")
-                    time.sleep(2)
-                else:
-                    stopper = player
-                    break
-            if stopper:
-                break
-
-        print(
-            f"\n{Fore.MAGENTA}*********************** Result !! ***********************"
-        )
-        display_cards(players)
-        print(f"{Fore.CYAN}Field sum is {game_state['sum']}")
-        print(f"{Fore.CYAN}Final call number is {game_state['call']}")
-        time.sleep(4)
-
-        stopper_index = players.index(stopper)
-        previous_player = players[stopper_index - 1]
-
-        if game_state["call"] > game_state["sum"]:
-            print(f"{Fore.RED}{previous_player.name} drops out!")
-            players.remove(previous_player)
-            if stopper_index == 0:
-                start_index = stopper_index
-            else:
-                start_index = stopper_index - 1
-        else:
-            print(f"{Fore.RED}{stopper.name} drops out!")
-            players.remove(stopper)
-            start_index = stopper_index - 1
-
-        time.sleep(1.5)
-
-    print(f"\n{Fore.MAGENTA}**********************************************")
-    print(f"{Fore.YELLOW}The winner is {players[0].name}!!!")
-    print(f"{Fore.MAGENTA}**********************************************")
-
-
-
+                return -1
